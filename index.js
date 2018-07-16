beb48f468089633fc0b8b3a4d098ad2fd181b90e
vikrantest <vsingh1918@gmail.com>
2018-07-16 18:35:57 +0530

    adding main inbox email

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express'),
 	nodemailer = require('nodemailer'),
 	bodyParser = require('body-parser'),
 	app = express();
-const http = require('http').Server(app);
+const http = require('http').Server(app),
+	sender_details = {email:'vikrant@vikrantsingh.xyz',password:'!@#singh1990'},
+	receiver = 'mainbox@vikrantsingh.xyz';
 
 http.listen(8800, function(){
   console.log('listening on *:8800..........................');
@@ -20,16 +22,17 @@ app.post('/mail/contact_me',function(req,res){
 	console.log(req.body);
 	let req_data = req.body;
 	let transporter = nodemailer.createTransport({
-	 service: 'gmail',
-	 auth: {
-			user: 'vikrant@vikrantsingh.xyz',
-			pass: '!@#singh1990'
+	service: 'gmail',
+	auth: {
+			user: sender_details.email,
+			pass: sender_details.password
 		}
 	});
+	const message = 'Name - '+req_data.name+' | Email - '
 	const mailOptions = {
-		from: 'vikrant@vikrantsingh.com', 
-		to: req_data.email, 
-		subject: req_data.name, 
+		from: sender_details.email, 
+		to: receiver, 
+		subject: message,
 		html: req_data.message
 	};
 	//fix handler with proper status
